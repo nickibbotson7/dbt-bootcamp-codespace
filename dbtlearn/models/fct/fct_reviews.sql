6b4b16f43d6c2a6f70539a166c6896d235d86cf5
nickibbotson7 <nickibbotson7@gmail.com>
2024-12-04 16:59:38 +0000

    commit

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -13,5 +13,12 @@ select
 from src_reviews
 where review_text is not null
 {% if is_incremental() %}
-  and review_date > (select max(review_date) from {{ this }})
+  {% if var("start_date",false) and var("end_date",false) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log ('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
